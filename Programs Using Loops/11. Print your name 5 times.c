898e7a12a65f8ba0e34dbfb50d02836088446909
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-09-28 16:22:54 +0530

    Create 11. Print your name 5 times.c

---
+++ Programs Using Loops/11. Print your name 5 times.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+int i;
+printf("My name is raj chandreshkumar, printing 5 times:\n");
+for (i = 0; i < 5; i++) {
+printf("Patel raj chandreshkumar\n");
+}
+return 0;
+}
