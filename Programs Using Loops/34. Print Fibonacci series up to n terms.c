357c542638f826b7efb4150a1107f3ff02cbc31a
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-09-28 16:47:02 +0530

    Create 34. Print Fibonacci series up to n terms.c

---
+++ Programs Using Loops/34. Print Fibonacci series up to n terms.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+int i, n, t1 = 0, t2 = 1, next_term;
+printf("Enter the number of terms: ");
+scanf("%d", &n);
+printf("Fibonacci series: ");
+for (i = 1; i <= n; ++i) {
+printf("%d, ", t1);
+next_term = t1 + t2;
+t1 = t2;
+t2 = next_term;
+}
+printf("\n");
+return 0;
+}
