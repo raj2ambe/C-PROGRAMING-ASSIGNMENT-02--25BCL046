c10ad6edb4ec989a9e89ac3cba1969b003b89e6d
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-09-28 16:23:53 +0530

    Create  12. Print your name n times.c

---
+++ Programs Using Loops/12. Print your name n times.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int i, n;
+printf("Enter a number (n) to print your name: ");
+scanf("%d", &n);
+for (i = 0; i < n; i++) {
+printf("Patel Om Kumar Rajendra bhai\n");
+}
+return 0;
+}
