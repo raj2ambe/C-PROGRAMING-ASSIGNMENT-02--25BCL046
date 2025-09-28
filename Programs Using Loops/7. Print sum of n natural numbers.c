9df5bcde10d7a4b2acd492feefed4f98a159a1eb
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-09-28 16:16:53 +0530

    Create 7. Print sum of n natural numbers.c

---
+++ Programs Using Loops/7. Print sum of n natural numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int i, n, sum = 0;
+printf("Enter a number (n): ");
+scanf("%d", &n);
+for (i = 1; i <= n; i++) {
+sum += i;
+}
+printf("Sum of first %d natural numbers is: %d\n", n, sum);
+return 0;
+}
