db49d962753b0b74e7c1408898f15e44b958d853
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-09-28 16:33:49 +0530

    Create  21. Separate digits of a given number.c

---
+++ Programs Using Loops/21. Separate digits of a given number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int num, digit;
+printf("Enter a number: ");
+scanf("%d", &num);
+while (num > 0) {
+digit = num % 10;
+printf("%d ", digit);
+num /= 10;
+}
+printf("\n");
+return 0;
+}
