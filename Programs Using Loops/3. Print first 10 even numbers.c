f58a3df871a67645030645c2324402eff2e88a71
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-09-28 16:12:52 +0530

    Create 3. Print first 10 even numbers.c

---
+++ Programs Using Loops/3. Print first 10 even numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int i, count = 0;
+printf("First 10 even numbers are:\n");
+for (i = 1; count < 10; i++) {
+if (i % 2 == 0) {
+printf("%d ", i);
+count++;
+}
+}
+printf("\n");
+return 0;
+}
