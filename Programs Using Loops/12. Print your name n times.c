d629f5ac9687d9f987600d6e4cc3b9f65413b180
rajpatel <92915725+raj2ambe@users.noreply.github.com>
2025-09-30 17:13:21 +0530

    Update 12. Print your name n times.c

--- Programs Using Loops/12. Print your name n times.c
+++ Programs Using Loops/12. Print your name n times.c
@@ -4,7 +4,7 @@ int i, n;
 printf("Enter a number (n) to print your name: ");
 scanf("%d", &n);
 for (i = 0; i < n; i++) {
-printf("Patel Om Kumar Rajendra bhai\n");
+printf("patel Raj Chandreshkumar\n");
 }
 return 0;
 }
